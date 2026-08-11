d5322fb8279244b26643a66304fc64e24a4959a2
Kaif <kaif.sheikh.0820@gmail.com>
2026-08-11 18:27:07 +0500

    Add cart item management functionalities: update item quantity, remove item, and clear cart

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -10,7 +10,7 @@ export const addToCart = async (req, res) => {
             return res.status(400).json({ success: false, message: "Product ID is required" });
         }
         if (quantity < 1) {
-            return res.status(400).json({ success: false, message: "Product quantity must be al least 1" });
+            return res.status(400).json({ success: false, message: "Product quantity must be atleast 1" });
         }
         // Check whether product actually exists
         const product = await Product.findById(productId);
@@ -74,6 +74,95 @@ export const getCart = async (req, res) => {
         return res.status(200).json({ success: true, cart, totalPrice });
     } catch (error) {
         console.log("Error in Get cart", error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const updateItemQuantity = async (req, res) => {
+    try {
+        const { productId } = req.params;
+        const { quantity } = req.body;
+        const userId = req.user.Id;
+
+        if (!quantity || quantity < 1) {
+            return res.status(400).json({ success: false, message: "Product quantity must be atleast 1" });
+        }
+        // Find the user cart
+        const cart = await Cart.findOne({ userId });
+        if (!cart) {
+            return req.status(404).json({ success: false, message: "Cart not found" });
+        }
+        // Find the product in the cart
+        const item = cart.items.find(item => item.productId.toString() === productId);
+        // If the product is not in cart
+        if (!item) {
+            return res.status(404).json({ success: false, message: "Product not found in the cart" });
+        }
+
+        // Update the quantity
+        item.quantity = quantity;
+
+        // Save Cart
+        await cart.save();
+
+        // Populate products information before sending responce
+        await cart.populate("items.productId");
+
+        return res.status(201).json({ success: true, message: "Cart updated successfully", cart });
+    } catch (error) {
+        console.log("Error in update cart", error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const removeFromCart = async (req, res) => {
+    try {
+        const { productId } = req.params;
+        const userId = req.user.Id;
+
+        const cart = await Cart.findOne({ userId });
+        if (!cart) {
+            return res.status(404).json({ success: false, message: "Cart not found" });
+        }
+
+        // Store current number of items in Cart
+        const oldLength = cart.items.length;
+
+        // Remove the product in the items array
+        cart.items = cart.items.filter(item => item.productId.toString() !== productId);
+
+        // Check whether anything was actually removed
+        if (cart.items.length === oldLength) {
+            return res.status(404).json({ success: false, message: "Product not found in cart" });
+        }
+
+        // Save the cart
+        await cart.save();
+
+        // Populate products information before sending responce
+        await cart.populate("items.productId");
+
+        return res.status(201).json({ success: true, message: "Product removed from cart", cart });
+    } catch (error) {
+        console.log("Error in Remove Product in the cart bt ID", error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const clearChart = async (req, res) => {
+    try {
+        const userId = req.user.Id;
+
+        const cart = await Cart.findOne({ userId });
+        if (!cart) {
+            return res.status(404).json({ success: false, message: "Cart not found" });
+        }
+        // Remove all the product in the cart
+        cart.items = [];
+        await cart.save();
+        return res.status(200).json({ success: true, message: "Cart clear successfully" });
+    } catch (error) {
+        console.log("Error in Clear Cart", error);
         return res.status(500).json({ success: false, message: error.message });
     }
 }
--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { getCart, addToCart } from '../controllers/cartController.js'
+import { addToCart, getCart, updateItemQuantity, removeFromCart, clearChart } from '../controllers/cartController.js'
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/', verifyToken, addToCart);
 router.get('/', verifyToken, getCart);
+router.put('/:productId', verifyToken, updateItemQuantity);
+router.delete('/:productId', verifyToken, removeFromCart);
+router.delete('/', verifyToken, clearChart);
 
 export default router;
